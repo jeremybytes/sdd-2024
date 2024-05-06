676a969eef6a20df2b41da18b6f3e1f6c02cbce6
Jeremy Clark <jeremy@jeremybytes.com>
2024-05-05 17:18:21 -0700

    Added dependency injection samples to workshop demos

---
+++ 06-csharp-workshop/DemoApps/DependencyInjection/PeopleViewer/Program.cs
@@ -0,0 +1,39 @@
+using PeopleViewer.Common;
+using PersonDataReader.CSV;
+using PersonDataReader.Decorators;
+using PersonDataReader.SQL;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.ConfigureKestrel(options => options.ListenLocalhost(5000));
+
+// Add services to the container.
+//builder.Services.AddSingleton<IPersonReader, CSVReader>();
+builder.Services.AddSingleton<IPersonReader>(s => GetReader());
+builder.Services.AddKeyedSingleton<IPersonReader, SQLReader>("method");
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
+static IPersonReader GetReader()
+{
+    return new CachingReader(new CSVReader());
+}
